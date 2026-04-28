2ca77149173b0ec4abac661396e4a211c0facf57
Amal <amalalikhardli@gmail.com>
2026-04-28 16:52:25 +0300

    -

--- src/layout/CopyRight.tsx
+++ src/layout/CopyRight.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { Heart } from "lucide-react";
+// import { Heart } from "lucide-react";
 
 const CopyRight = () => {
   const { t, i18n } = useTranslation();
